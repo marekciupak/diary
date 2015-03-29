6e0311038d483bc823fc5f8659436ac6d2ac397f
Gregor <gregor.afc@gmail.com>
2015-03-29 12:01:31 +0200

    Add refernces Subject to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329094952) do
+ActiveRecord::Schema.define(version: 20150329100000) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "division_id"
@@ -46,7 +46,10 @@ ActiveRecord::Schema.define(version: 20150329094952) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "subjects", ["user_id"], name: "index_subjects_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

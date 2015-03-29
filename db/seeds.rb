11e77cf91a8a3da323346fe5d2afab7f2e86dd4f
Gregor <gregor.afc@gmail.com>
2015-03-29 12:20:31 +0200

    Add User validations and some seeds for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
+User.destroy_all
 Subject.destroy_all
+
+
+User.create!([
+  { first_name: 'admin', last_name: 'admin', email: 'admin@example.com', password: 'test1234' },
+  { first_name: 'teacher', last_name: 'teacher', email: 'teacher@example.com', password: 'test1234' },
+  { first_name: 'student', last_name: 'student', email: 'student@example.com', password: 'test1234' }
+])
+p "Created #{User.count} users."
+
+
+
 Subject.create!([
   { name: "English", description: "English course"},
   { name: "Math", description: "Math course"},

05cb81a7d10e76d4e980d6388e78629f912cfcfc
Gregor <gregor.afc@gmail.com>
2015-03-29 13:00:09 +0200

    Seeds

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,6 +1,6 @@
 class Division < ActiveRecord::Base
   has_many :assignments
   has_many :subjects, through: :assignments
-  has_many :user
+  has_many :users
   has_many :grades, through: :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,27 @@
 User.destroy_all
 Subject.destroy_all
+Division.destroy_all
 
 
-User.create!([
-  { first_name: 'admin', last_name: 'admin', email: 'admin@example.com', password: 'test1234' },
-  { first_name: 'teacher', last_name: 'teacher', email: 'teacher@example.com', password: 'test1234' },
-  { first_name: 'student', last_name: 'student', email: 'student@example.com', password: 'test1234' }
-])
+
+user_admin = User.create!( first_name: 'admin', last_name: 'admin', email: 'admin@example.com', password: 'test1234' )
+user_teacher = User.create!( first_name: 'teacher', last_name: 'teacher', email: 'teacher@example.com', password: 'test1234' )
+user_student = User.create!( first_name: 'student', last_name: 'student', email: 'student@example.com', password: 'test1234' )
 p "Created #{User.count} users."
 
+division_1 = Division.create!( year: '2000', letter: 'A' )
+division_2 = Division.create!( year: '2000', letter: 'B' )
+division_3 = Division.create!( year: '2000', letter: 'C' )
+p "Created #{Division.count} users."
 
+user_student.division_id = division_1.id
+user_student.save
+p 'User_student have division'
 
-Subject.create!([
-  { name: "English", description: "English course"},
-  { name: "Math", description: "Math course"},
-  { name: "Polish", description: "Polish course"}  
-])
- 
+subject_1 = Subject.create!(name: "English", description: "English course")
+subject_2 = Subject.create!(name: "Math", description: "Math course")
+subject_3 = Subject.create!(name: "Polish", description: "Polish course")
 p "Created #{Subject.count} subjects."
+
+subject_1.user_id = user_teacher.id
+subject_1.save

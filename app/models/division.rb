f0144ec53bee93bdc63ca8e91541803d3b8295bc
Marek Ciupak <ciupakm@gmail.com>
2015-03-29 12:14:42 +0200

    relations update

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,2 +1,5 @@
 class Division < ActiveRecord::Base
+  has_many :assignments
+  has_many :subjects, through: :assignments
+  has_one :user
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,7 @@
 class Subject < ActiveRecord::Base
+  has_many :appointments
+  has_many :assignments, through: :assignments
+
   validates :name, presence: true, length: { minimum: 4 }
   validates :description, presence:true, length: {minimum: 10}
 end
